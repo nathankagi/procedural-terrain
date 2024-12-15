abb55f239e08ff229241a27ab4ab0a450584f795
Nathan Kagi <npkagi@outlook.com>
2024-12-15 09:21:47 +0100

    terrain api changes

--- src/terrain.rs
+++ src/terrain.rs
@@ -5,10 +5,10 @@ const MAX_LAYER_COUNT: i32 = 100;
 pub struct Terrain {
     width: usize,
     height: usize,
-    map: Vec<Vec<Block>>,
+    cells: Vec<Vec<Cell>>,
 }
 
-#[derive(Copy, Clone, Default)]
+#[derive(Copy, Clone)]
 pub struct Material {
     erosion: f32,
     cohesion: f32,
@@ -23,8 +23,7 @@ pub struct Layer {
 }
 
 #[derive(Clone)]
-pub struct Block {
-    height: f32,
+pub struct Cell {
     layers: Vec<Layer>,
 }
 
@@ -36,20 +35,44 @@ impl Terrain {
         Terrain {
             width,
             height,
-            map: vec![vec![Block::new(); width]; height],
+            cells: vec![vec![Cell::new(); width]; height],
         }
     }
 
     fn height(&self, x: usize, y: usize) -> f32 {
-        return self.map[x][y].height;
+        return self.cells[x][y].height();
     }
 
     fn material(&self, x: usize, y: usize) -> Material {
-        return self.map[x][y].layers.last().unwrap().material;
+        return self.cells[x][y].layers.last().unwrap().material;
     }
 
     fn normal(&self, x: usize, y: usize) -> Vector3<f32> {
-        return Vector3::new(0.0, 0.0, 0.0);
+        let height_l = if x > 0 {
+            self.cells[x - 1][y].height()
+        } else {
+            self.cells[x][y].height()
+        };
+
+        let height_r = if x < self.width - 1 {
+            self.cells[x + 1][y].height()
+        } else {
+            self.cells[x][y].height()
+        };
+
+        let height_d = if y > 0 {
+            self.cells[x][y - 1].height()
+        } else {
+            self.cells[x][y].height()
+        };
+
+        let height_u = if y < self.height - 1 {
+            self.cells[x][y + 1].height()
+        } else {
+            self.cells[x][y].height()
+        };
+
+        return Vector3::new(height_l - height_r, 2.0, height_d - height_u).normalize();
     }
 
     fn gradient(&self, x: usize, y: usize) -> Vector3<f32> {
@@ -57,58 +80,71 @@ impl Terrain {
     }
 
     fn add(&self, x: usize, y: usize, layer: Layer) -> () {
-        if ()
+        return ();
     }
 
-    fn remove(&self, x: usize, y: usize, height: f32) -> Vec<Layer> {
-        let h = self.map[x][y].height;
+    fn remove(&mut self, x: usize, y: usize, height: f32) -> Vec<Layer> {
+        // while height > 0
+        // subtract top layer height from height, add to return layers
 
-        let layers: Vec<Layer> = if (height < h) {
-            self.map[x][y].layers.last().unwrap().height = self.map[x][y].layers.last().unwrap().height - height;
+        // let h = self.cells[x][y].height();
 
-            return vec![Layer {
-                height,
-                material: self.map[x][y].layers.last().unwrap().material,
-            }];
+        // let layers: Vec<Layer> = if height < h {
+        //     let h = Some(self.cells[x][y].layers.last().unwrap().height);
+        //     // self.cells[x][y].layers.last().unwrap().height =
+        //     //     self.cells[x][y].layers.last().unwrap().height - height;
 
-        } else if height == h {
-            return vec![self.map[x][y].layers.pop().unwrap()];
+        //     vec![Layer {
+        //         height,
+        //         material: self.cells[x][y].layers.last().unwrap().material,
+        //     }]
+        // } else if height == h {
+        //     vec![self.cells[x][y].layers.pop().unwrap()]
+        // } else if height > h {
+        //     let l = self.cells[x][y].layers.pop().unwrap();
 
-        } else if height > h {
-            let l = self.map[x][y].layers.pop().unwrap();
+        //     // self.cells[x][y].layers.last().unwrap().height =
+        //     //     self.cells[x][y].layers.last().unwrap().height - height;
 
-            self.map[x][y].layers.last().unwrap().height = self.map[x][y].layers.last().unwrap().height - height;
+        //     vec![
+        //         l,
+        //         Layer {
+        //             height,
+        //             material: self.cells[x][y].layers.last().unwrap().material,
+        //         },
+        //     ]
+        // } else {
+        //     return vec![];
+        // };
 
-            return vec![l, Layer {
-                height,
-                material: self.map[x][y].layers.last().unwrap().material,
-            }];
-        }
-        else {
-            return vec![];
-        }
-
-        return layers;
+        // return layers;
     }
 
-    fn top(&self, x: usize, y: usize) -> &Layer {
-        return &self.map[x][y].layers.last().unwrap();
+    fn top(&self, x: usize, y: usize) -> Option<&Layer> {
+        return self.cells[x][y].layers.last();
     }
 }
 
-impl Block {
+impl Cell {
     fn new() -> Self {
-        Block {
-            height: 0.0,
+        Cell {
             // layers: vec![Layer::new(), MAX_LAYER_COUNT],
             layers: Vec::new(),
         }
     }
+
+    fn height(&self) -> f32 {
+        let mut h: f32 = 0.0;
+        for layer in self.layers.iter() {
+            h = h + layer.height;
+        }
+        return h;
+    }
 }
 
-// impl Layer {
-//     fn new() -> Self {}
-// }
+impl Layer {
+    fn new() -> Self {}
+}
 
 impl Layer {}
 
